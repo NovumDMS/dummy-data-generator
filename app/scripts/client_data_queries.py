5be5eeb71f88cc1ed363f6cf6a2ee77ecafa0fa8
devmodo1230 <devmodo1019@gmail.com>
2026-03-24 18:13:19 -0600

    Fix to Queries

--- app/scripts/client_data_queries.py
+++ app/scripts/client_data_queries.py
@@ -8,22 +8,42 @@ def client_customer_query() -> str:
             AND delete_flag = 'N';
     """
 
-def client_item_query() -> str:
+def client_data_query() -> str:
     return f"""
-        SELECT L.inv_mast_uid, M.item_id, M.item_desc, M.buyer_id
+        WITH default_buyer AS (
+            SELECT
+                id,
+                contact_name
+            FROM p21s_contacts
+            WHERE buyer = 'Y'
+            ORDER BY id
+            LIMIT 1
+        )
+        SELECT 
+            S.supplier_id, 
+            S.supplier_name, 
+            COALESCE(C.id, DB.id) AS buyer_id, 
+            COALESCE(C.contact_name, DB.contact_name) AS buyer_name, 
+            M.inv_mast_uid, 
+            M.item_desc
         FROM p21s_inv_mast M
-            JOIN p21s_inv_loc L ON L.inv_mast_uid = M.inv_mast_uid
+        JOIN p21s_inv_loc L 
+            ON M.inv_mast_uid = L.inv_mast_uid
+        JOIN p21s_inventory_supplier X 
+            ON M.inv_mast_uid = X.inv_mast_uid
+        JOIN p21s_inventory_supplier_x_loc W 
+            ON X.inventory_supplier_uid = W.inventory_supplier_uid
+        AND L.location_id = W.location_id
+        AND W.primary_supplier = 'Y'
+        JOIN p21s_supplier S 
+            ON X.supplier_id = S.supplier_id
+        LEFT JOIN p21s_contacts C 
+            ON C.id = S.buyer_id::INTEGER
+        CROSS JOIN default_buyer DB
         WHERE L.stockable = 'Y'
-            AND L.sellable = 'Y'
-            AND L.buy = 'Y'
-            AND L.qty_on_hand > 0;
-    """
-
-def client_supplier_query(item_id: int) -> str:
-    return f"""
-        SELECT S.supplier_id, S.buyer_id, S.supplier_name
-        FROM p21s_supplier S
-            JOIN p21s_inventory_supplier I ON I.supplier_id = S.supplier_id
-        WHERE I.inv_mast_uid = {item_id}
-            AND S.delete_flag = 'N';
+        AND L.sellable = 'Y'
+        AND L.buy = 'Y'
+        AND L.qty_on_hand > 0
+        AND L.delete_flag = 'N'
+        AND S.delete_flag = 'N';
     """

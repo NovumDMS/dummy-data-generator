eb42064f3caa5ba0af06fb68f066fafad6568c77
devmodo1230 <devmodo1019@gmail.com>
2026-04-08 17:05:27 -0600

    Minor changes to use Zip

--- app/helper/file_helper.py
+++ app/helper/file_helper.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import csv
+import zipfile
 from sqlalchemy.orm import Session
 
 import logging
@@ -23,7 +24,11 @@ def generate_tsv_file(data: list[dict], db: Session, file_prefix: str) -> None:
 
     # Resolve project root (two levels up: app/routes -> app -> project root)
     project_root = Path(__file__).resolve().parents[2]
-    tsv_dir = project_root / "tsv_files" / "sales_orders"
+    tsv_dir = project_root / "tsv_files"
+    if file_prefix == "SOH" or file_prefix == "SOL":
+        tsv_dir = tsv_dir / "sales_orders"
+    elif file_prefix == "POH" or file_prefix == "POL":
+        tsv_dir = tsv_dir / "purchase_orders"
     tsv_dir.mkdir(parents=True, exist_ok=True)
 
     file_name = f"{file_prefix}_{import_set_no}.tsv"
@@ -36,3 +41,21 @@ def generate_tsv_file(data: list[dict], db: Session, file_prefix: str) -> None:
             writer.writerow([row.get(col, "") for col in columns])
 
     logger.info(f"Generated TSV file at {file_path}")
+
+def zip_orders(tsv_dir: Path, output_zip: Path) -> Path:
+    files_to_delete = []
+
+    with zipfile.ZipFile(output_zip, "w", compression=zipfile.ZIP_DEFLATED) as z:
+        for file in (tsv_dir / "sales_orders").glob("*.tsv"):
+            z.write(file, arcname=f"Sales Orders/{file.name}")
+            files_to_delete.append(file)
+
+        for file in (tsv_dir / "purchase_orders").glob("*.tsv"):
+            z.write(file, arcname=f"Purchase Orders/{file.name}")
+            files_to_delete.append(file)
+
+    for file in files_to_delete:
+        file.unlink()
+
+    return output_zip
+    
